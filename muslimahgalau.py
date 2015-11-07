fa30cb538785e9b87c817d714b84b297db0b5e48
umar khotman <xhijack@gmail.com>
2015-11-07 22:57:31 +0700

    change account

--- muslimahgalau.py
+++ muslimahgalau.py
@@ -8,10 +8,10 @@ __email__="ramdani@sopwer.net"
 __date__ ="$Nov 1, 2015 13:50:18 PM$"
 
 
-ACCESS_TOKEN = '2260316470.6c7aa75.1b8a372b7cda43e29370097445ac31f3'
+ACCESS_TOKEN = '2268381288.6c7aa75.8671fc30723c404fa6ad51bde2854a3b'
 CLIENT_SECRET = 'b8f0d7112d3b40bd9927cfd28bf2a422'
 USERNAME = 'muslimahgalau'
-PASSWORD = 'master'
+PASSWORD = 'master88'
 
 USERS = ['RUMAHDAKWAH', 'BERTAHAJUDLAH', 'KANGABAY_', 'INDONESIAMENUTUPAURAT', 'FELIXSIAUW',
          'NIKAHBAROKAH', 'MUDA_BERDAKWAH', 'MENIKAHBAHAGIA', 'JODOH.SEJATI', 'DAKWAHJOMBLO', 'CINTAMULIA',
@@ -24,11 +24,11 @@ class MuslimahGalau(Instapy):
     instagram = InstagramAPI(access_token=ACCESS_TOKEN, client_secret=CLIENT_SECRET)
     insta_post = InstagramSession(username=USERNAME, password=PASSWORD)
     user_followed = USERS
-    tags = ['#muslimahgalau', '#muslimah', '#galau', '#akhowat']
+    tags = ['#muslimahgalau', '#muslimah', '#galau', '#akhowat', '#akhwat', '#tausiyah']
     IMAGES_PATH = '/home/ramdani/projects/auto-repost/muslimah_images/'
 
 if __name__ == '__main__':
     print "Start Reposting"
-    mg = MuslimahGalau(waiting_time=random.randint(100, 300))
+    mg = MuslimahGalau(waiting_time=random.randint(100, 600))
     mg.post_media_from_choice()
     print "Finished"
